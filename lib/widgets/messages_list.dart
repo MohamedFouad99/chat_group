0f620d7ea9f4ae6b192c8cb439c14820465dff01
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2023-03-15 18:39:43 +0200

    replace_streamBuilder_to_bloc

--- lib/widgets/messages_list.dart
+++ lib/widgets/messages_list.dart
@@ -1,40 +1,31 @@
-import 'package:chat_group/screens/chat_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore_for_file: must_be_immutable
+
+import 'package:chat_group/cubits/chat_cubit/chat_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'message_line.dart';
 
 class MessagesList extends StatelessWidget {
-  const MessagesList({super.key});
-
+  MessagesList({super.key});
+  List<MessageLine> messageWidgets = [];
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: firestore.collection('messages').orderBy('time').snapshots(),
-      builder: (context, snapshot) {
-        List<MessageLine> messageWidgets = [];
-        if (!snapshot.hasData) {}
-        final messages = snapshot.data!.docs.reversed;
-        for (var message in messages) {
-          final messageText = message.get('text');
-          final messageSender = message.get('sender');
-          final currentUser = signedInUser.email;
-
-          final messageWidget = MessageLine(
-            sender: messageSender,
-            text: messageText,
-            isMe: currentUser == messageSender,
-          );
-          messageWidgets.add(messageWidget);
-        }
-        return Expanded(
-          child: ListView(
+    return Expanded(
+      child: BlocConsumer<ChatCubit, ChatState>(
+        listener: (context, state) {
+          if (state is ChatSuccess) {
+            messageWidgets = state.messageWidgets;
+          }
+        },
+        builder: (context, state) {
+          return ListView(
             reverse: true,
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             children: messageWidgets,
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
